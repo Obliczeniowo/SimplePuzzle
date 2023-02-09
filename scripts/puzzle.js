f08cc0432c3ac1a6e7122d2855e548bc0709e415
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2023-02-09 03:39:42 +0100

    rename variables

--- scripts/puzzle.js
+++ scripts/puzzle.js
@@ -11,29 +11,29 @@ function Point2d(x, y) {
 	};
 }
 
-let divPuzle;
-let imgPuzle;
+let divPuzzle;
+let imgPuzzle;
 let myCanvas;
-let dcontext;
-let selindex;
+let dContext;
+let selIndex;
 let pos;
 let dx;
 let dy;
 let onPosition;
 
-let tpos = new Array();
+let tPos = new Array();
 
 function RotateImage(angle, img, x, y, dx, dy) {
 
 	angle = Math.PI / 180 * angle;
 
 	let p1 = new Point2d(dx, dy);
-	dcontext.save();
+	dContext.save();
 	p1.Rotate(angle);
-	dcontext.translate(-p1.x + x, -p1.y + y);
-	dcontext.rotate(angle);
-	dcontext.drawImage(img, 0, 0);
-	dcontext.restore();
+	dContext.translate(-p1.x + x, -p1.y + y);
+	dContext.rotate(angle);
+	dContext.drawImage(img, 0, 0);
+	dContext.restore();
 }
 
 function puzel(image, index, x, y) {
@@ -41,36 +41,36 @@ function puzel(image, index, x, y) {
 	this.image = image;
 	this.index = index;
 	this.angle = Math.random() * 360;
-	this.onpos = false;
+	this.onPos = false;
 
 	this.draw = function () {
 		RotateImage(this.angle, this.image, this.pos.x, this.pos.y, dx, dy);
 	}
 }
 
-let puzle = new Array();
+let puzzle = new Array();
 
 let index = new Array();
 
 function draw() {
-	dcontext.clearRect(0, 0, myCanvas.width, myCanvas.height);
-	if (selindex == -1) {
+	dContext.clearRect(0, 0, myCanvas.width, myCanvas.height);
+	if (selIndex == -1) {
 		for (let i = 0; i < 16; i++) { //
-			if (!puzle[i].onpos)
-				puzle[i].draw();
+			if (!puzzle[i].onPos)
+				puzzle[i].draw();
 		}
 	} else {
-		RotateImage(0, imgPuzle[16], pos.x, pos.y, 0, 0);
+		RotateImage(0, imgPuzzle[16], pos.x, pos.y, 0, 0);
 		for (let i = 0; i < 16; i++) { //
-			if (puzle[i].onpos)
-				puzle[i].draw();
+			if (puzzle[i].onPos)
+				puzzle[i].draw();
 		}
-		puzle[selindex].draw();
+		puzzle[selIndex].draw();
 
 	}
 	if (onPosition == 16) {
 		for (let i = 0; i < 16; i++) { //
-			puzle[i].draw();
+			puzzle[i].draw();
 		}
 	}
 }
@@ -84,11 +84,11 @@ function getMousePos(e) {
 
 function initialize() {
 
-	divPuzle = document.getElementById("puzle");
-	imgPuzle = divPuzle.getElementsByTagName("img");
+	divPuzzle = document.getElementById("puzzle");
+	imgPuzzle = divPuzzle.getElementsByTagName("img");
 	myCanvas = document.getElementById("drawing");
-	dcontext = myCanvas.getContext('2d');
-	selindex = -1;
+	dContext = myCanvas.getContext('2d');
+	selIndex = -1;
 
 	pos = new Point2d((myCanvas.width - 600) / 2, (myCanvas.height - 400) / 2);
 
@@ -96,37 +96,37 @@ function initialize() {
 	dy = 75;
 	onPosition = 0;
 	let i = 0;
-	tpos[i] = new Point2d(pos.x + dx, pos.y + dy);
+	tPos[i] = new Point2d(pos.x + dx, pos.y + dy);
 	i++
-	tpos[i] = new Point2d(pos.x + dx + 143, pos.y + dy);
+	tPos[i] = new Point2d(pos.x + dx + 143, pos.y + dy);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 271, pos.y + dy);
+	tPos[i] = new Point2d(pos.x + dx + 271, pos.y + dy);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 450, pos.y + dy);
+	tPos[i] = new Point2d(pos.x + dx + 450, pos.y + dy);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx, pos.y + dy + 59);
+	tPos[i] = new Point2d(pos.x + dx, pos.y + dy + 59);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 150, pos.y + dy + 78);
+	tPos[i] = new Point2d(pos.x + dx + 150, pos.y + dy + 78);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 288, pos.y + dy + 87);
+	tPos[i] = new Point2d(pos.x + dx + 288, pos.y + dy + 87);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 450, pos.y + dy + 77);
+	tPos[i] = new Point2d(pos.x + dx + 450, pos.y + dy + 77);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx, pos.y + dy + 200);
+	tPos[i] = new Point2d(pos.x + dx, pos.y + dy + 200);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 113, pos.y + dy + 160);
+	tPos[i] = new Point2d(pos.x + dx + 113, pos.y + dy + 160);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 290, pos.y + dy + 190);
+	tPos[i] = new Point2d(pos.x + dx + 290, pos.y + dy + 190);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 439, pos.y + dy + 179);
+	tPos[i] = new Point2d(pos.x + dx + 439, pos.y + dy + 179);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx, pos.y + dy + 260);
+	tPos[i] = new Point2d(pos.x + dx, pos.y + dy + 260);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 141, pos.y + dy + 290);
+	tPos[i] = new Point2d(pos.x + dx + 141, pos.y + dy + 290);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 264, pos.y + dy + 260);
+	tPos[i] = new Point2d(pos.x + dx + 264, pos.y + dy + 260);
 	i++;
-	tpos[i] = new Point2d(pos.x + dx + 411, pos.y + dy + 297);
+	tPos[i] = new Point2d(pos.x + dx + 411, pos.y + dy + 297);
 
 	for (let i = 0; i < 16; i++) {
 		index[i] = i;
@@ -141,7 +141,7 @@ function initialize() {
 	}
 
 	for (let i = 0; i < 16; i++) { //
-		puzle[i] = new puzel(imgPuzle[index[i]], index[i], (i % 6) * 150 + 150, Math.floor(i / 6) * 200 + 150);
+		puzzle[i] = new puzel(imgPuzzle[index[i]], index[i], (i % 6) * 150 + 150, Math.floor(i / 6) * 200 + 150);
 	}
 
 	draw();
@@ -159,40 +159,40 @@ function initialize() {
 			x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(myCanvas.offsetLeft);
 			y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop - Math.floor(myCanvas.offsetTop) + 1;
 
-			if (selindex == -1) {
+			if (selIndex == -1) {
 				let dist = 10000;
-				selindex = -1;
+				selIndex = -1;
 				let dist2;
 				for (let i = 0; i < 16; i++) {
-					if (!puzle[i].onpos) {
-						dist2 = Math.sqrt(Math.pow(x - puzle[i].pos.x, 2) + Math.pow(y - puzle[i].pos.y, 2));
+					if (!puzzle[i].onPos) {
+						dist2 = Math.sqrt(Math.pow(x - puzzle[i].pos.x, 2) + Math.pow(y - puzzle[i].pos.y, 2));
 						if (dist2 < dist) {
 							dist = dist2;
-							selindex = i;
+							selIndex = i;
 
 						}
 					}
 				}
-				puzle[selindex].angle = 0;
+				puzzle[selIndex].angle = 0;
 			} else {
-				if (Math.sqrt(Math.pow(tpos[puzle[selindex].index].x - puzle[selindex].pos.x, 2) + Math.pow(tpos[puzle[selindex].index].y - puzle[selindex].pos.y, 2)) < 50) {
-					puzle[selindex].pos.x = tpos[puzle[selindex].index].x;
-					puzle[selindex].pos.y = tpos[puzle[selindex].index].y;
-					if (!puzle[selindex].onpos) {
-						puzle[selindex].onpos = true;
+				if (Math.sqrt(Math.pow(tPos[puzzle[selIndex].index].x - puzzle[selIndex].pos.x, 2) + Math.pow(tPos[puzzle[selIndex].index].y - puzzle[selIndex].pos.y, 2)) < 50) {
+					puzzle[selIndex].pos.x = tPos[puzzle[selIndex].index].x;
+					puzzle[selIndex].pos.y = tPos[puzzle[selIndex].index].y;
+					if (!puzzle[selIndex].onPos) {
+						puzzle[selIndex].onPos = true;
 						onPosition++;
 						if (onPosition == 16) {
 							alert("Ty draniu wygrałeś!!!");
 						}
 					}
 				} else {
-					puzle[selindex].pos.x = x;
-					puzle[selindex].pos.y = y;
+					puzzle[selIndex].pos.x = x;
+					puzzle[selIndex].pos.y = y;
 				}
 
-				puzle[selindex].angle = 0;
+				puzzle[selIndex].angle = 0;
 
-				selindex = -1;
+				selIndex = -1;
 			}
 
 			draw();
@@ -200,18 +200,18 @@ function initialize() {
 	}
 
 	myCanvas.onmousemove = function (e) {
-		if (selindex > -1) {
+		if (selIndex > -1) {
 			x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(myCanvas.offsetLeft);
 			y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop - Math.floor(myCanvas.offsetTop) + 1;
-			puzle[selindex].pos.x = x;
-			puzle[selindex].pos.y = y;
+			puzzle[selIndex].pos.x = x;
+			puzzle[selIndex].pos.y = y;
 		}
 		if (onPosition < 16)
 			draw();
 	}
 
 	myCanvas.onMouseUp = function (e) {
-		selindex = -1;
+		selIndex = -1;
 	}
 }
 
